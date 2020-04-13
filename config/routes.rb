58b7c0158433b4c81af66280da29360ef448963f
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2020-04-13 19:28:10 +0200

    New team model & index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teams/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'pages#home'
+  resources :teams, only:[:index]
 end
